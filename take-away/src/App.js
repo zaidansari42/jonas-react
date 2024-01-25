ab7b995df88dd5977dfec14a24edd27fa1400b8a
Zaid Ansari <zaidansari42@gmail.com>
2024-01-25 21:16:38 +0530

    Refactored code by removing passsing of states in props

--- take-away/src/App.js
+++ take-away/src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -6,12 +8,31 @@ const initialItems = [
 ];
 
 export default function App() {
+  // state
+  const [totalItems, setTotalItems] = useState(initialItems);
+
+  const handleItems = (item) => {
+    setTotalItems([...totalItems, item]);
+  };
+
+  const UpdateAfterCheck = (updatedItems) => {
+    setTotalItems(updatedItems);
+  };
+  const UpdateAfterDelete = (updatedItems) => {
+    setTotalItems(updatedItems);
+  };
+
   return (
     <div className="app">
       <Header />
-      <FormBar />
-      <List />
-      <Footer />
+      <FormBar handleItems={handleItems} />
+      <List
+        UpdateAfterCheck={UpdateAfterCheck}
+        UpdateAfterDelete={UpdateAfterDelete}
+        totalItems={totalItems}
+        setTotalItems={setTotalItems}
+      />
+      <Footer totalItems={totalItems} />
     </div>
   );
 }
@@ -20,34 +41,90 @@ function Header() {
   return <h1>🏝️ FAR AWAY 🧳</h1>;
 }
 
-function FormBar() {
-  const handleSubmit = () => {};
+function FormBar({ handleItems }) {
+  // States
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  // Handling Form Submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    handleItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
+  // Handling Change
+  const handleChange = (value) => {
+    setDescription(value);
+  };
+
+  const handleOption = (value) => {
+    setQuantity(value);
+  };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-      <select id="mySelect" name="mySelect">
+      <select
+        id="mySelect"
+        name="mySelect"
+        onChange={(e) => handleOption(e.target.value)}
+        value={quantity}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option>{num}</option>
+          <option key={num}>{num}</option>
         ))}
       </select>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(e) => handleChange(e.target.value)}
+        value={description}
+      />
       <button>Add</button>
     </form>
   );
 }
 
-function List() {
+function List({ UpdateAfterCheck, UpdateAfterDelete, totalItems }) {
+  // Handling Functions
+  const handleChecked = (checked, productId) => {
+    const updatedItems = totalItems.map((el) => {
+      if (el.id === productId) {
+        return { ...el, packed: checked };
+      }
+      return el;
+    });
+    UpdateAfterCheck(updatedItems);
+  };
+
+  const handleDelete = (productId) => {
+    const updatedItems = totalItems.filter((el) => el.id !== productId);
+
+    UpdateAfterDelete(updatedItems);
+  };
+
+  // Use Effect
+
   return (
     <div className="list">
       <ul>
-        {initialItems.map((el) => (
-          <li>
-            <input type="checkbox" />
+        {totalItems.map((el) => (
+          <li key={el.id}>
+            <input
+              type="checkbox"
+              checked={el.packed}
+              onChange={(e) => handleChecked(e.target.checked, el.id)}
+            />
             <span style={el.packed ? { textDecoration: "line-through" } : {}}>
               {el.quantity} {el.description}
             </span>
-            <button>❌</button>
+            <button onClick={(e) => handleDelete(el.id)}>❌</button>
           </li>
         ))}
       </ul>
@@ -55,10 +132,18 @@ function List() {
   );
 }
 
-function Footer() {
+function Footer({ totalItems }) {
+  const packedItems = totalItems.filter((item) => item.packed);
+  const percentageOfPackedItems = Math.ceil(
+    (packedItems.length / totalItems.length) * 100
+  );
+
   return (
     <footer className="stats">
-      <em>You Have X items on your list and you have already packed x (X%)</em>
+      <em>
+        You Have {totalItems?.length} items on your list and you have already
+        packed {packedItems.length} ({percentageOfPackedItems}%)
+      </em>
     </footer>
   );
 }
